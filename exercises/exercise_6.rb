1b93d141b4708e01092883e0b2521abc8e1b1455
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-09 10:51:56 -0500

    Complete ethe exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# Add the following code directly inside the Store model (class): has_many :employees
+Store.has_many :employees
+
+# Add the following code directly inside the Employee model (class): belongs_to :store
+Employee.belongs_to :store
+
+# Add some data into employees. 
+@store1.employees.create(first_name: "Astarina", last_name: "Maulida", hourly_rate: 70)
+@store1.employees.create(first_name: "Marco", last_name: "Gamal", hourly_rate: 80)
+@store1.employees.create(first_name: "Kioko", last_name: "Khaleesha", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Sora", last_name: "Eugenie", hourly_rate: 50)
+@store2.employees.create(first_name: "Dewi", last_name: "Rahayu", hourly_rate: 60)
+@store2.employees.create(first_name: "Bambang", last_name: "Hartanto", hourly_rate: 70)
