0bd11f11a42be771c57b6423de37bef8dc3cfb7f
Astarina Maulida <astarina.maulida@gmail.com>
2021-11-09 11:13:50 -0500

    Complete the exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,14 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+# Ask the user for a store name (store it in a variable)
+puts "Enter a store name"
+store_name = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields
+new_store = Store.create(name: store_name)
+
+# Display the error messages provided back from ActiveRecord to the user 
+# (one on each line) after you attempt to save/create the record
+error_message = new_store.errors.messages
+puts "Error #{error_message}! Failed to save the record."
